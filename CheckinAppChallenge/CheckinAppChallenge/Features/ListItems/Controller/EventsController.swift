79d20717a4361c01e31cf5ff0ba7e97fe8c15274
Renan Trapp <trapprenan@gmail.com>
2018-12-09 15:19:26 -0200

    Added Models to project and load values from server

---
+++ CheckinAppChallenge/CheckinAppChallenge/Features/ListItems/Controller/EventsController.swift
@@ -0,0 +1,29 @@
+//
+//  EventsController.swift
+//  CheckinAppChallenge
+//
+//  Created by Renan Trapp on 09/12/18.
+//  Copyright © 2018 Renan Trapp. All rights reserved.
+//
+
+import Foundation
+
+class EventsController {
+    typealias CompletionRequestAPI = (Bool, [Event]?, Error?) -> Void
+
+    func getListOfEvents(url: String, completion: @escaping CompletionRequestAPI){
+        Service.request(url: url) { (result) in
+            switch result {
+            case .success(let data):
+                do {
+                    let events = try JSONDecoder().decode([Event].self, from: data)
+                    completion(true, events, nil)
+                } catch(let error) {
+                    completion(true, nil, error)
+                }
+            case .failure(let error):
+                completion(false,nil,error)
+            }
+        }
+    }
+}
---
+++ CheckinAppChallenge/CheckinAppChallenge/Models/People.swift
@@ -0,0 +1,16 @@
+//
+//  People.swift
+//  CheckinAppChallenge
+//
+//  Created by Renan Trapp on 09/12/18.
+//  Copyright © 2018 Renan Trapp. All rights reserved.
+//
+
+import Foundation
+
+struct People: Codable {
+    var id: String
+    var eventId: String
+    var name: String
+    var picture: String?
+}
